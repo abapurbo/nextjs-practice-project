597c40872b0c1adbc5d4107f16ae3985ffeae559
apurbo <absarker238@gmail.com>
2025-12-26 14:54:56 +0600

    added signup page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Login() {
+    return <div>
+        <h1>Sign in your account</h1>
+    </div>;
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  return <div>
-    <h1>🐬 Dev-Story</h1>
-  </div>;
+    return <div>
+        <nav className="flex justify-between items-center px-10 py-2">
+            <h1 className="text-3xl font-bold">🐬 Dev-Story</h1>
+            <div className="flex gap-6">
+               <Link href='/'>Home</Link>
+               <Link href='/'>Tutorials</Link>
+               <Link href='/'>Stories</Link>
+               <Link href='/'>About</Link>
+               <Link href='/'>Dashboard</Link>
+               <Link href='/'>Login</Link>
+               <Link href='/'>Sign Up</Link>
+            </div>
+        </nav>
+    </div>;
 }
